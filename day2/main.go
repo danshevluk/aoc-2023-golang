c2fd22d8762e44c08bbbcf943637dc8b46566670
Dan Shevluk <dan.shevluk@hey.com>
2023-12-20 01:07:15 +0000

    Day 2 part 2

---
+++ day2/main.go
@@ -0,0 +1,155 @@
+package main
+
+import (
+	"aoc_2023/common"
+	"errors"
+	"flag"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type Cube byte
+
+const (
+	Red Cube = iota
+	Green
+	Blue
+)
+
+var cubeCounts = map[Cube]int{
+	Red:   12,
+	Green: 13,
+	Blue:  14,
+}
+
+func cubeFromString(str string) (Cube, error) {
+	switch str {
+	case "red":
+		return Red, nil
+	case "green":
+		return Green, nil
+	case "blue":
+		return Blue, nil
+	default:
+		return Red, errors.New("invalid cube name")
+	}
+}
+
+func main() {
+	flag.Parse()
+	inputContent := common.ReadInputFile()
+
+	fmt.Println("Possible games IDs sum:", solvePart1(inputContent))
+	fmt.Println("Sum of powers for min valid sets:", solvePart2(inputContent))
+}
+
+func solvePart1(inputContent string) int {
+	possibleGameIDs := make([]string, 0)
+
+Lines:
+	for _, line := range strings.Split(inputContent, "\n") {
+		lineParts := strings.Split(line, ":")
+		if len(lineParts) != 2 {
+			fmt.Println("Invalid line:", line)
+			continue
+		}
+
+		// Get game id
+		gameInfoPart := strings.Split(lineParts[0], " ")
+		gameID := gameInfoPart[len(gameInfoPart)-1]
+
+		// Get game data
+		attempts := strings.Split(lineParts[1], ";")
+		for _, cubesData := range attempts {
+			cubesDataParts := strings.Split(cubesData, ",")
+			for _, cubeInfo := range cubesDataParts {
+				infoItems := strings.Split(strings.TrimSpace(cubeInfo), " ")
+				if len(infoItems) != 2 {
+					fmt.Println("Invalid cube info:", cubeInfo)
+					continue Lines
+				}
+
+				count, err := strconv.Atoi(infoItems[0])
+				if err != nil {
+					fmt.Println("Invalid cube count:", infoItems[0])
+					continue Lines
+				}
+				cubeName, err := cubeFromString(infoItems[1])
+				if err != nil {
+					fmt.Println("Invalid cube name:", infoItems[1])
+					continue Lines
+				}
+				if count > cubeCounts[cubeName] {
+					// Impossible game
+					continue Lines
+				}
+			}
+		}
+		possibleGameIDs = append(possibleGameIDs, gameID)
+	}
+
+	// Sum all possible game IDs
+	result := 0
+	for _, gameID := range possibleGameIDs {
+		gameIDInt, err := strconv.Atoi(gameID)
+		if err != nil {
+			fmt.Println("Invalid game ID:", gameID)
+			continue
+		}
+		result += gameIDInt
+	}
+
+	return result
+}
+
+func solvePart2(inputContent string) int {
+	powersOfMinCubeSets := make([]int, 0)
+
+Lines:
+	for _, line := range strings.Split(inputContent, "\n") {
+		lineParts := strings.Split(line, ":")
+		if len(lineParts) != 2 {
+			fmt.Println("Invalid line:", line)
+			continue
+		}
+
+		// Get game data
+		attempts := strings.Split(lineParts[1], ";")
+		maxCubeValues := make(map[Cube]int, 0)
+		for _, cubesData := range attempts {
+			cubesDataParts := strings.Split(cubesData, ",")
+			for _, cubeInfo := range cubesDataParts {
+				infoItems := strings.Split(strings.TrimSpace(cubeInfo), " ")
+				if len(infoItems) != 2 {
+					fmt.Println("Invalid cube info:", cubeInfo)
+					continue Lines
+				}
+
+				count, err := strconv.Atoi(infoItems[0])
+				if err != nil {
+					fmt.Println("Invalid cube count:", infoItems[0])
+					continue Lines
+				}
+				cubeName, err := cubeFromString(infoItems[1])
+				if err != nil {
+					fmt.Println("Invalid cube name:", infoItems[1])
+					continue Lines
+				}
+
+				maxCubeValues[cubeName] = max(maxCubeValues[cubeName], count)
+			}
+		}
+
+		powersOfMinCubeSets = append(
+			powersOfMinCubeSets,
+			maxCubeValues[Red]*maxCubeValues[Green]*maxCubeValues[Blue],
+		)
+	}
+
+	result := 0
+	for _, item := range powersOfMinCubeSets {
+		result += item
+	}
+	return result
+}
