468298b6d530f62f89fbb8283035c6de7409716d
Dan Shevluk <dan.shevluk@hey.com>
2023-12-21 22:14:04 +0000

    Day 4 part 2

--- day4/main.go
+++ day4/main.go
@@ -15,7 +15,10 @@ func main() {
 	inputContent := common.ReadInputFile()
 
 	fmt.Println("Part 1 solution: ", solvePart1(inputContent))
+	fmt.Println("Part 2 solution: ", solvePart2(inputContent))
 }
+
+// Part 1
 
 func solvePart1(input string) int {
 	lines := strings.Split(input, "\n")
@@ -77,4 +80,88 @@ func solvePart1(input string) int {
 	}
 
 	return gamesListScore
+}
+
+// Part 2
+
+func solvePart2(input string) int {
+	lines := strings.Split(input, "\n")
+
+	numberOfTicketsByType := make([]int, len(lines))
+	for i := range numberOfTicketsByType {
+		numberOfTicketsByType[i] = 1
+	}
+
+	for lineIndex, line := range lines {
+		lineParts := strings.Split(line, ":")
+		if len(lineParts) != 2 {
+			fmt.Println("Invalid line:", line)
+			continue
+		}
+
+		numberSets := strings.Split(lineParts[1], "|")
+		if len(numberSets) != 2 {
+			fmt.Println("Invalid number sets:", numberSets)
+			continue
+		}
+		numberSetToArray := func(numberSet string) []int {
+			numberSet = strings.TrimSpace(numberSet)
+			if len(numberSet) == 0 {
+				return make([]int, 0)
+			}
+
+			numbersArray := make([]int, 0)
+			for _, number := range strings.Split(numberSet, " ") {
+				number = strings.TrimSpace(number)
+				if len(number) == 0 {
+					continue
+				}
+
+				numberInt, err := strconv.Atoi(number)
+				if err != nil {
+					fmt.Println("Invalid number:", number)
+					continue
+				}
+
+				numbersArray = append(numbersArray, numberInt)
+			}
+
+			return numbersArray
+		}
+
+		winningNumbers := numberSetToArray(numberSets[0])
+		cardNumbers := numberSetToArray(numberSets[1])
+
+		foundWinningNumbersCount := 0
+		for _, cardNumber := range cardNumbers {
+			if slices.Contains(winningNumbers, cardNumber) {
+				foundWinningNumbersCount += 1
+			}
+		}
+
+		if foundWinningNumbersCount == 0 {
+			continue
+		}
+
+		// Save new ticket counts that we just won
+		nextTicketIndex := lineIndex + 1
+		if nextTicketIndex >= len(lines) {
+			continue
+		}
+
+		// Each of tickets wins plus one for each next ticket
+		numberOfTicketsOfThisType := numberOfTicketsByType[lineIndex]
+		for index := range numberOfTicketsByType[nextTicketIndex:min(nextTicketIndex+foundWinningNumbersCount, len(lines))] {
+			ticketTypeIndex := nextTicketIndex + index
+			numberOfTicketsByType[ticketTypeIndex] += numberOfTicketsOfThisType
+		}
+	}
+
+	totalNumberOfTickets := 0
+	for _, numberOfTickets := range numberOfTicketsByType {
+		totalNumberOfTickets += numberOfTickets
+
+	}
+
+	return totalNumberOfTickets
 }
